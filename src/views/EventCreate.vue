e74d6eb0db66c0f6fe2bceab631e8edf5c18747c
Sean <spward@coastal.edu>
2020-03-19 13:57:33 -0400

    VueX Started

---
+++ src/views/EventCreate.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event is created by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
+    <p>{{ getEventById(2) }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters(["getEventById"]),
+    ...mapState(["user", "categories"]),
+    catLength() {
+      return this.$store.getters.catLength;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/EventList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="event-list">
+    <h1>Events Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService.js";
+
+export default {
+  name: "EventList",
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: []
+    };
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data;
+      })
+      .catch(error => {
+        console.log("There was an error:", error.response);
+      });
+  }
+};
+</script>
